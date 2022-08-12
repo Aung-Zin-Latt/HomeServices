8f9222d38f268de8fb655652c58c8250c1d95b0d
Aung-Zin-Latt <aungzinlatt007@gmail.com>
2022-08-12 22:09:50 +0630

    first commit

---
+++ app/Http/Livewire/Admin/AdminServiceCategoryComponent.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Livewire\Admin;
+
+use App\Models\ServiceCategory;
+use Livewire\Component;
+use Livewire\WithPagination;
+
+class AdminServiceCategoryComponent extends Component
+{
+    use WithPagination;
+    public function render()
+    {
+        $scategories = ServiceCategory::orderBy('id', 'DESC')->paginate(10);
+
+        return view('livewire.admin.admin-service-category-component', [
+            'scategories' => $scategories,
+        ])->layout('layouts.base');
+    }
+}
---
+++ routes/web.php
@@ -0,0 +1,45 @@
+<?php
+
+use App\Http\Livewire\Admin\AdminAddServiceCategoryComponent;
+use App\Http\Livewire\Admin\AdminDashboardComponent;
+use App\Http\Livewire\Admin\AdminServiceCategoryComponent;
+use App\Http\Livewire\Customer\CustomerDashboardComponent;
+use App\Http\Livewire\HomeComponent;
+use App\Http\Livewire\ServiceCategoriesComponent;
+use App\Http\Livewire\Sprovider\SproviderDashboardComponent;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+Route::get('/', HomeComponent::class)->name('home');
+Route::get('/service-categories', ServiceCategoriesComponent::class)->name('home.service_categories');
+
+// For Customer
+Route::middleware(['auth:sanctum',config('jetstream.auth_session'),'verified'])->group(function () {
+    Route::get('/customer/dashboard', CustomerDashboardComponent::class)->name('customer.dashboard');
+});
+
+// For Service Provider
+Route::middleware(['auth:sanctum',config('jetstream.auth_session'),'verified', 'authsprovider'])->group(function () {
+    Route::get('/sprovider/dashboard', SproviderDashboardComponent::class)->name('sprovider.dashboard');
+});
+
+// For Admin
+Route::middleware(['auth:sanctum',config('jetstream.auth_session'),'verified', 'authadmin'])->prefix('admin')->group(function () {
+    Route::get('/dashboard', AdminDashboardComponent::class)->name('admin.dashboard');
+    Route::get('/service-categories', AdminServiceCategoryComponent::class)->name('admin.service_categories');
+    Route::get('/service-category/add', AdminAddServiceCategoryComponent::class)->name('admin.add_service_category');
+});
